4de273e72e1d84858abceb14d0600438b142fe6b
unknown <ST10494767@imconnect.edu.za>
2025-11-12 22:09:41 +0200

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { SafeAreaView, View, Text, FlatList, Pressable, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 type MenuItem = {
   name: string;
@@ -97,6 +99,20 @@ export function FilterScreen({ route, navigation }: any) {
     </SafeAreaView>
   );
 }
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {/* Define your app screens here */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   // screen wrapper like HomeScreen
